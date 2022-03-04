d599de60ac289b9e4f8c30d595742e4037685cf6
PetrAnisimov <petranisimov812@gmail.com>
2022-03-04 10:36:36 +0300

    commit with Math. Operator

---
+++ src/hw1/MathOperators.java
@@ -0,0 +1,9 @@
+package hw1;
+
+public class MathOperators {
+    public static void main(String[] args) {
+        int num = 99, result = num/10+num%10;
+
+        System.out.println(result);
+    }
+}
